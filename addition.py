0067fed81c8edd62ba3018eb81d896edeba3e3ff
adifulpagar <adityafulpagar21@gmail.com>
2023-03-01 20:58:42 +0530

    This is my second commit

--- addition.py
+++ addition.py
@@ -2,3 +2,22 @@ def addition(a, b):
     return a+b
 
 print(addition(1, 2))
+
+print("This line is newly added")
+
+"""
+git status
+git add addition.py
+git commit - m "This is my frist commit"
+git config --global user.email "adityafulpagar21@gmail.com"
+git config --global user.name "adifulpagar"
+git commit - m "This is my fiorst commit"
+git push origin main
+"""
+
+"""
+git diff    -- press "q" to come out
+git add
+git status
+
+"""
